0e33017c6ae45094e1ebd42ecf1caaa72d2c3aa7
JI HUN LEE <51016231+easyhooon@users.noreply.github.com>
2024-02-19 19:06:04 +0900

    [FEAT] Entity data class @Stable marker 적용

--- core/domain/src/main/kotlin/com/leejihun/supergene/assignment/domain/entity/RandomUserEntity.kt
+++ core/domain/src/main/kotlin/com/leejihun/supergene/assignment/domain/entity/RandomUserEntity.kt
@@ -1,28 +1,35 @@
 package com.leejihun.supergene.assignment.domain.entity
 
+import androidx.compose.runtime.Stable
+
+@Stable
 data class RandomUserEntity(
     val results: List<UserInfoEntity>,
     val info: InfoEntity,
 )
 
+@Stable
 data class UserInfoEntity(
     val name: UserNameEntity,
     val email: String,
     val picture: UserPictureEntity,
 )
 
+@Stable
 data class UserNameEntity(
     val title: String,
     val first: String,
     val last: String,
 )
 
+@Stable
 data class UserPictureEntity(
     val large: String,
     val medium: String,
     val thumbnail: String,
 )
 
+@Stable
 data class InfoEntity(
     val seed: String,
     val results: Int,
